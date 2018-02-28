ae49ee7584a9ca1edf173b9bda5975e93d99fb1c
Udo Sauer <fernsehmuell@googlemail.com>
2018-02-28 21:44:16 +0100

    first version of the script, barely tested.

--- Scripts/ultraschall_set_next_marker_to_planning_stage.lua
+++ Scripts/ultraschall_set_next_marker_to_planning_stage.lua
@@ -25,10 +25,14 @@
 ]]
  
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> first version of the script, barely tested.
 local info = debug.getinfo(1,'S');
 script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
 dofile(script_path .. "ultraschall_helper_functions.lua")
 planned_color = ultraschall.ConvertColor(100,255,0)
+<<<<<<< HEAD
 
 function set_next_marker_to_planning_stage()
   cursor_pos=reaper.GetCursorPosition()
@@ -60,9 +64,46 @@ end
 set_next_marker_to_planning_stage()
 
 =======
+=======
+>>>>>>> first version of the script, barely tested.
 
--- Print Message to console (debugging)
-function Msg(val)
-  reaper.ShowConsoleMsg(tostring(val).."\n")
+function get_position()
+    playstate=reaper.GetPlayState() --0 stop, 1 play, 2 pause, 4 rec possible to combine bits
+    if playstate & 1 == 1 or playstate & 4==4 then
+        return reaper.GetPlayPosition()
+    else
+        return reaper.GetCursorPosition()
+    end
 end
+<<<<<<< HEAD
 >>>>>>> initial empty script with copyright header and debug function
+=======
+
+function set_next_marker_to_planning_stage()
+  cursor_pos=get_position()
+  -- iterate over list of all markerz
+  retval, num_markers, num_regions = reaper.CountProjectMarkers(0) -- get number of all markers
+  
+  if num_markers>0 then
+    retval, isrgn, last_pos, rgnend, name, markrgnindexnumber, color = reaper.EnumProjectMarkers3(0, 0) -- get first marker
+    if last_pos>=cursor_pos then last_marker_index=0 else last_marker_index=-1 end
+    
+    for i=1,num_markers-1 do --loop through all markers and find the next marker relative to cursor position
+      retval, isrgn, pos, rgnend, name, markrgnindexnumber, color = reaper.EnumProjectMarkers3(0, i)
+      if ((pos >= cursor_pos) and (last_marker_index>-1) and (pos<last_pos)) or ((pos >= cursor_pos) and (last_marker_index==-1)) then
+        last_pos=pos
+        last_marker_index=i
+      end
+    end
+    
+    if last_marker_index>-1 then
+      reaper.Undo_BeginBlock()
+        retval, isrgn, pos, rgnend, name, markrgnindexnumber, color = reaper.EnumProjectMarkers3(0, last_marker_index)
+        reaper.SetProjectMarker4(0, markrgnindexnumber, false, pos, 0, name, planned_color|0x1000000, 0)        
+      reaper.Undo_EndBlock("Ultraschall - Set next marker to planning stage)", -1)  
+    end
+  end
+end
+
+set_next_marker_to_planning_stage()
+>>>>>>> first version of the script, barely tested.
