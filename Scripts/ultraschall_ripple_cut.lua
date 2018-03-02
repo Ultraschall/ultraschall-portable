b7092f501dbb023a210f6f474f38a33795b5091e
Ralf Stockmann <rstockm@gwdg.de>
2018-03-02 01:00:43 +0100

    Refactoring
    Aufrufkette sind in einer Funktion gebündelt
    Helper Functions eingebaut
    Datum angepasst
    Formatierung angepasst
    Fehlertext angepasst

--- Scripts/ultraschall_ripple_cut.lua
+++ Scripts/ultraschall_ripple_cut.lua
@@ -1,7 +1,7 @@
 --[[
 ################################################################################
 # 
-# Copyright (c) 2014-2017 Ultraschall (http://ultraschall.fm)
+# Copyright (c) 2014-2018 Ultraschall (http://ultraschall.fm)
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,24 +23,28 @@
 # 
 ################################################################################
 ]]
- 
+
 -------------------------------------
--- Print Message to console (debugging)
+-- Ultraschall Helper Functions
 -------------------------------------
 
-function Msg(val)
-  reaper.ShowConsoleMsg(tostring(val).."\n")
+local info = debug.getinfo(1,'S');
+script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
+dofile(script_path .. "ultraschall_helper_functions.lua")
+
+-------------------------------------
+-- Set of Actions to Copy and Ripple Cut
+-------------------------------------
+
+function RippleCut()
+ 	
+ 	reaper.Main_OnCommand(40061, 0)						-- Split all Items at time selection
+	reaper.Main_OnCommand(40717, 0)						-- Select all items in time selection
+	reaper.Main_OnCommand(41383, 0)						-- Copy all items within time selection to clipboard
+	reaper.Main_OnCommand(40201, 0)						-- Ripple cut Selection
+  reaper.Main_OnCommand(40289, 0)						-- Unselect all Items 
+
 end
-
--------------------------------------
--- run a command by its name
--------------------------------------
-
-function runcommand(cmd)	
-	start_id = reaper.NamedCommandLookup(cmd)
-	reaper.Main_OnCommand(start_id,0) 
-end
-
 
 -------------------------------------
 reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
@@ -49,23 +53,14 @@ reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of y
 init_start_timesel, init_end_timesel = reaper.GetSet_LoopTimeRange(0, 0, 0, 0, 0)
 
 if init_end_timesel ~= init_start_timesel then		-- there is a time selection
-	reaper.Main_OnCommand(40061, 0)						-- Split all Items at time selection
-	reaper.Main_OnCommand(40717, 0)						-- Select all items in time selection
-	reaper.Main_OnCommand(41383, 0)						-- Copy all items within time selection to clipboard
-	reaper.Main_OnCommand(40201, 0)						-- Ripple cut Selection
-  reaper.Main_OnCommand(40289, 0)						-- Unselect all Items
-
+	RippleCut()
 
 elseif reaper.CountSelectedMediaItems(0) == 1 then	-- exacty one item selected
 	runcommand("_SWS_SAFETIMESEL")						-- Set time selection to item borders
-	reaper.Main_OnCommand(40061, 0)						-- Split all Items at time selection
-	reaper.Main_OnCommand(40717, 0)						-- Select all items in time selection
-	reaper.Main_OnCommand(41383, 0)						-- Copy all items within time selection to clipboard
-	reaper.Main_OnCommand(40201, 0)						-- Ripple cut Selection
-	reaper.Main_OnCommand(40289, 0)						-- Unselect all Items
+	RippleCut()
 
 else 													-- no time selection or items selected
- 	result = reaper.ShowMessageBox( "No time selection active", "Ultraschall Ripple Cut", 0 )	-- Info window
+ 	result = reaper.ShowMessageBox( "You need to make a time selection or to select a single item to ripple-cut.", "Ultraschall Ripple Cut", 0 )	-- Info window
 end
 
 -------------------------------------
