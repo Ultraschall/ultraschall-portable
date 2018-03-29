28b6967ffbfa339433d9be845cd19176f3255239
lootsy <21980459+lootsy@users.noreply.github.com>
2018-03-29 06:16:06 +0200

    remove deletion of niceDiffed reaperKbIni file; let it always be present

--- UsTeamHelpers/sort_reaper_kb_ini.sh
+++ UsTeamHelpers/sort_reaper_kb_ini.sh
@@ -10,17 +10,8 @@ REAPER_KB_INI="reaper-kb.ini"
 WORKING_DIRECTORY=$PWD
 cd "$(git rev-parse --show-cdup)" #go to toplevel directory in this repository
 
-# create niceDiff folder if it does not exist
+# create folder "niceDiff" if it does not exist
 [ ! -d "$NICE_DIFF_FOLDER" ] && mkdir -p "$NICE_DIFF_FOLDER"
-
-# remove file if staged file is marked for deletion
-for fileToDelete in $(git diff --cached --name-status | grep ^D | cut -f2)
-do
-    if [ -e $fileToDelete ] #file exists
-    then
-        rm "$NICE_DIFF_FOLDER/$fileToDelete"
-    fi
-done
 
 # do something for staged file that is marked as modified
 for modifiedFile in $(git diff --cached --name-status | grep ^M | cut -f2)
