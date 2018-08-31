32a62e863342e27ecab278f9060bf90e99c244c5
Udo Sauer <fernsehmuell@googlemail.com>
2018-08-31 15:48:48 +0200

    Dieses Beispielscript aktiviert das Metronom und beendet es wieder, wenn der Playcursor einen Marker passiert hat.

--- Scripts/ultraschall_metronome_background.lua
+++ Scripts/ultraschall_metronome_background.lua
@@ -10,6 +10,13 @@ dofile(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")
 
 
 function US_play_sound_using_metronome()
+  -- enable 41745
+  -- disable 41746
+  lastmetronome=reaper.time_precise()
+  reaper.Main_OnCommand(41745,0)
+  metronome=1
+  reaper.ShowConsoleMsg("Metronome=ON\r")
+  --reaper.Main_OnCommand(41746,0)
 end
 
 function US_get_nearest_markers_from_position()
@@ -33,18 +40,38 @@ end
 
 
 function MainLoop()
-    if reaper.time_precise()-lasttime > 1 then
+    if metronome==1 and reaper.time_precise()-lastmetronome > BEEP_DUR then
+      reaper.ShowConsoleMsg("Metronome=OFF\r")
+      reaper.Main_OnCommand(41746,0) --stop metronome
+      lastmetronome=0
+      metronome=0
+    end
+    if reaper.time_precise()-lasttime > .1 then
       lasttime=reaper.time_precise()
       marker=US_get_nearest_markers_from_position()
-      if marker ~="" then
-        reaper.ShowConsoleMsg("Ein Marker! Er hat den Namen: "..marker.."\r")
+      if lasttime-lastmarkertime>1 then lastmarker="" end
+      if marker ~="" and marker ~= lastmarker then
+        lastmarker=marker
+        lastmarkertime=lasttime
+        US_play_sound_using_metronome()
+        --reaper.ShowConsoleMsg("Ein Marker! Er hat den Namen: "..marker.."\r")
       end
+      if marker==lastmarker then lastmarkertime=reaper.time_precise() end
     end
     reaper.defer(MainLoop)
 end
 
 
+BEEP_DUR=2
+function theEnd()
+end
+reaper.atexit(theEnd)
 
-lasttime = reaper.time_precise() + 1
+
+lasttime = reaper.time_precise() + .1
+lastmarker=""
+lastmetronome=0
+metronome=0
+lastmarkertime=lasttime
 reaper.defer(MainLoop)
 
