2d18638cc00c2ab27844db429245e7e0d38645e7
Meo Mespotine <lspmp3@yahoo.de>
2019-02-08 17:53:34 +0100

    Ultraschall-Portable_Januar2019-RippleInsertRecording
    Ultraschall-Portable_Januar2019-RippleInsertRecording
        Scripts/ultraschall_InsertRecording.lua
        UserPlugins/ultraschall_api/ultraschall_hotfixes.lua
        reaper-kb.ini
            Erlaubt es einem eine Aufnahme einzufügen, ohne dabei zusätzliche Takes zu erstellen.
            Das ist praktisch, wenn man einen Podcast macht und beim Abhören merkt, "Oh, da fehlt aber noch ein Satz".
            Es splittet alle Spuren an der NeuAufnahme-Stelle, verschiebt alles was danach kommt um 4 Sekunden nach hinten und
            startet Aufnahme.
            Wenn die Lücke zu klein würde, verschiebt es alles nach hinten, so dass Items nie überschrieben werden.
            Wenn man die Aufnahme beendet, wird die Lücke wieder geschlossen.
    
            Auch Marker/Regions und Envelope-punkte werden verschoben(hierzu ist es wichtig die ultraschall_hotfixes.lua reinzunehmen).
    
            Ich habs testweise gelegt auf die Tastenkombination Alt+Shift+R.

---
+++ Scripts/ultraschall_InsertRecording.lua
@@ -0,0 +1,82 @@
+--[[
+################################################################################
+# 
+# Copyright (c) 2014-2019 Ultraschall (http://ultraschall.fm)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# 
+################################################################################
+]]
+
+-- Meo Mespotine 5th of February 2019
+
+-- Inserts recording at EditCursorposition
+-- will keep a gap until recording is stopped and close the gap after that
+-- good for "Of, I forgot to add this or that to include in the middle of my recording"-usecases
+
+dofile(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")
+
+function MoveUntilStop()
+  -- moves items, track-envelope-points, regions and markers after current recording-position to be always at least 
+  -- 4 seconds away until recording stops
+  if position<=reaper.GetPlayPosition()+4 and reaper.GetPlayState()&2==0 then
+    retval = ultraschall.MoveMediaItemsAfter_By(position, 4, trackstring)
+    retval = ultraschall.MoveMarkersBy(position, reaper.GetProjectLength()+1, 4, false)
+    retval = ultraschall.MoveRegionsBy(position, reaper.GetProjectLength(), 4, false)
+    for i=0, reaper.CountTracks(0)-1 do
+      ultraschall.MoveTrackEnvelopePointsBy(position, reaper.GetProjectLength()+1, 4, reaper.GetTrack(0,i), false)
+    end
+    position=position+4
+  end  
+  if reaper.GetPlayState()&4==4 then reaper.defer(MoveUntilStop) else 
+    -- if recording stops, move items, trackenvelope-points, regions and markers after current recording-position
+    -- back to close the 4+ second gap
+    stopposition=reaper.GetPlayPosition()
+    Aretval = ultraschall.MoveMediaItemsAfter_By(position, -(position-stopposition), trackstring)
+    retval = ultraschall.MoveMarkersBy(position, reaper.GetProjectLength()+1, -(position-stopposition), false)
+    for i=0, reaper.CountTracks(0)-1 do
+      ultraschall.MoveTrackEnvelopePointsBy(position, reaper.GetProjectLength()+1, -(position-stopposition), reaper.GetTrack(0,i), false)
+    end
+    reaper.UpdateArrange()
+  end
+end
+
+if reaper.GetPlayState()~=0 then 
+  reaper.CSurf_OnStop() 
+  --elseif reaper.GetCursorPosition()>=reaper.GetProjectLength() then
+  --  reaper.CSurf_OnRecord()
+else
+  -- get currently existing tracks and current edit-cursorposition
+  trackstring=ultraschall.CreateTrackString_AllTracks()
+  position=reaper.GetCursorPosition()
+
+  -- move items, trackenvelope-points, regions and markers after current editcursor-position by 
+  -- 4 seconds toward the end of the project to make room for the recording
+  retval, MediaItemArray = ultraschall.SplitMediaItems_Position(position, trackstring, false)
+  retval = ultraschall.MoveMediaItemsAfter_By(position, 4, trackstring)
+  retval = ultraschall.MoveMarkersBy(position, reaper.GetProjectLength()+1, 4, false)
+  for i=0, reaper.CountTracks(0)-1 do
+    ultraschall.MoveTrackEnvelopePointsBy(position, reaper.GetProjectLength()+1, 4, reaper.GetTrack(0,i), false)
+  end
+  
+  position=position+4
+  reaper.CSurf_OnRecord()
+  MoveUntilStop()  
+end
+
