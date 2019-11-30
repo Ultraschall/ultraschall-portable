ce622e02006d389d1697e8c785c00abb7511028d
Udo Sauer <fernsehmuell@googlemail.com>
2019-11-30 21:00:36 +0100

    Added audio playback also on reverse shuttle mode.

--- Scripts/ultraschall_shuttle_background_script.lua
+++ Scripts/ultraschall_shuttle_background_script.lua
@@ -23,6 +23,16 @@
 # 
 ################################################################################
 ]]
+dofile(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")
+
+function ultraschall.Scrubbing_GetToggleState()
+  if reaper.SNM_GetIntConfigVar("scrubmode", -9)&1==0 then return false else return true end
+end
+
+function ultraschall.Scrubbing_Toggle(toggle)
+  if type(toggle)~="boolean" then ultraschall.AddErrorMessage("Scrubbing_Toggle", "toggle", "must be boolean", -1) return false end
+  return ultraschall.GetSetIntConfigVar("scrubmode", true, toggle)
+end
 
 function get_position()
     playstate=reaper.GetPlayState() --0 stop, 1 play, 2 pause, 4 rec possible to combine bits
@@ -58,7 +68,9 @@ function check_position() --check if cursor is past the last edit, if so goto to
 end
 
 function init()
+    scrubstate=ultraschall.Scrubbing_GetToggleState()
     if (reaper.GetPlayState() & 4)==4 then return end --stop if recording!!
+    A,B=ultraschall.Scrubbing_Toggle(true)
     ignoreonexit=false
     speed_list = {1,2,3,5,8,20,40,100}
     max_speed=#speed_list
@@ -67,16 +79,16 @@ function init()
     reaper.SetProjExtState(0, "Ultraschall", "Reverse_Play_Shuttle", 1) -- store state in datastore
     starttime=reaper.time_precise() --get actual time
     check_position() -- move cursor if necessary
-    reaper.OnPlayButton()
+    reaper.Main_OnCommand(1016,0) --stop
+    reaper.Main_OnCommand(1008,0) --pause    
 end
 
 function onexit()
+    A,B=ultraschall.Scrubbing_Toggle(scrubstate)
     if ignoreonexit==false then
         reaper.SetProjExtState(0, "Ultraschall", "Reverse_Play_Shuttle", 0) -- store state in datastore
         if (reaper.GetPlayState() & 4)==4 then return end --exit if recording!!
         reaper.Main_OnCommand(40521, 0) -- set play speed to 1
-        -- beobachten, ob das doch wichtig war: reaper.Main_OnCommand(1007,0) --play
-        -- beobachten, ob das doch wichtig war: reaper.Main_OnCommand(1008,0) --pause
         reaper.Main_OnCommand(1016,0) --stop
     end
     if (reaper.GetPlayState() & 4)==4 then return end --exit if recording!!
@@ -94,13 +106,11 @@ function runloop() --BACKGROUND Loop
         starttime=reaper.time_precise() --get actual time after speedchange!
         reaper.SetProjExtState(0, "Ultraschall", "Reverse_Play_Shuttle", 1) -- store state in datastore
         start_position=get_position()
-        reaper.Main_OnCommand(1008,0) --pause
     end
 
     if playing_reverse_state()==1 and ( (reaper.GetPlayState() & 1==1) or (reaper.GetPlayState() & 2==2) ) and reaper.GetCursorPosition()>0 then --reverse playing -> move cursor
         time_passed=(reaper.time_precise()-starttime) * speed_list[speed]
         reaper.MoveEditCursor(start_position-time_passed-reaper.GetCursorPosition(), 0)
-        reaper.OnPlayButton()
         reaper.CSurf_OnPlayRateChange(1.0+speed_list[speed]/10000.0+0.00005) -- encode speed in Playratevalue 1x -> 1.0001, 100x -> 1.0100
         reaper.defer(runloop) -- restart loop
     end
@@ -108,7 +118,6 @@ function runloop() --BACKGROUND Loop
     if playing_reverse_state()==3 then --fwd shuttle play was pressed: stop reverse playing and play forward
         reaper.Main_OnCommand(40521, 0) -- set play speed to 1
         reaper.SetProjExtState(0, "Ultraschall", "Reverse_Play_Shuttle", 0) -- store state in datastore    
-        -- beobachten, ob das vielleicht doch wichtig war: reaper.Main_OnCommand(1008,0) --pause
         reaper.Main_OnCommand(1016,0) --stop
         reaper.OnPlayButton() --play
         ignoreonexit=true
