dbe86d1f306e00fb924edf1de3fb357e77857d26
Meo Mespotine <lspmp3@yahoo.de>
2020-02-01 00:24:45 +0100

    Clearing Up

--- README.md
+++
@@ -1,2 +0,0 @@
-# ultraschall-portable
-Portable ULTRASCHALL Podcasting Extensions including the REAPER DAW 
--- Scripts/ultraschall_Add_ExampleScripts_To_Reaper.lua
+++
@@ -1,7 +0,0 @@
-dofile(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")
-
-filecount, files = ultraschall.GetAllFilenamesInPath(ultraschall.Api_Path.."/Scripts_Examples")
-
-for i=1, filecount do
-  reaper.AddRemoveReaScript(true, 0, files[i], true)
-end
