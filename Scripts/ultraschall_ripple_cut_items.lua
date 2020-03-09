8110a9d87ed5e55802035a0e78e861a0f9745e23
Ralf Stockmann <rstockm@gmail.com>
2020-03-09 15:06:02 +0100

    Update ultraschall_ripple_cut_items.lua
    Edge Case für überlappende Icons abgefangen. Sollte zur 4.1 noch mal sauberer implementiert werden.

--- Scripts/ultraschall_ripple_cut_items.lua
+++ Scripts/ultraschall_ripple_cut_items.lua
@@ -62,15 +62,19 @@ if selected_items_count > 0 then  -- at least one item selected
   selectedItems = GetAllSelectedItems(selected_items_count) -- baue ein Array mit allen selektierten Items. Da nach einem Ripple-Cut die Selektionen alle weg sind, kommt man so auch an die nachfolgenden Items ran.
 
   for i = 1, #selectedItems do   -- INITIALIZE loop through selected items
+
     item = selectedItems[i] -- Get selected item i
+    if reaper.ValidatePtr2(0, item, "MediaItem*") == true then  -- liefert false, wenn das item durch eine Überdeckung zu einem vorherigen schon zerschnitten wurde. Edge-Case der nochmal gefixt werden sollte.
 
-    start_of_item_position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-    length = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
-    end_of_item_position = start_of_item_position + length
+      start_of_item_position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+      length = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+      end_of_item_position = start_of_item_position + length
 
-    reaper.GetSet_LoopTimeRange(true, false, start_of_item_position, end_of_item_position, false) -- setzt die Zeitauswahl auf die Kanten des items
-    CommandNumber = reaper.NamedCommandLookup("_Ultraschall_Ripple_Cut") -- der bekannte Ripple-Cut einer Zeitauswahl mit Sauce und scharf, Erhalt von Envelope-Points etc.
-    reaper.Main_OnCommand(CommandNumber,0)
+      reaper.GetSet_LoopTimeRange(true, false, start_of_item_position, end_of_item_position, false) -- setzt die Zeitauswahl auf die Kanten des items
+      CommandNumber = reaper.NamedCommandLookup("_Ultraschall_Ripple_Cut") -- der bekannte Ripple-Cut einer Zeitauswahl mit Sauce und scharf, Erhalt von Envelope-Points etc.
+      reaper.Main_OnCommand(CommandNumber,0)
+
+    end
 
   end -- ENDLOOP through selected items
 
