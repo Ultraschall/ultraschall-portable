6263715fedd1362ddd6deac77c9eb237e1467ab2
Udo Sauer <fernsehmuell@googlemail.com>
2020-05-26 17:32:20 +0200

    StreamDeck Script. Loads another Script which is part of the StreamDeck Plugin. Additional Line in reaper.kb-ini: SCR 4 0 Ultraschall_StreamDeck "Custom: Ultraschall: Streamdeck" ultraschall_streamdeck.lua

---
+++ Scripts/ultraschall_streamdeck.lua
@@ -0,0 +1,7 @@
+if string.sub(reaper.GetOS(),1,3)=="OSX" then
+  dofile(os.getenv("HOME").."/Library/Application Support/com.elgato.StreamDeck/Plugins/fm.ultraschall.ultradeck.sdPlugin/LUA/Ultraschall_StreamDeck_2.lua")
+elseif string.sub(reaper.GetOS(),1,3)=="Win" then
+  dofile(os.getenv("APPDATA")..[[\Elgato\StreamDeck\Plugins\com.elgato.controlcenter.sdPlugin\Ultraschall_StreamDeck_2.lua]])
+else
+  reaper.ShowMessageBox("Stream Deck Plugin is only available on MacOS and Windows", "Wrong OS", 0)
+end
