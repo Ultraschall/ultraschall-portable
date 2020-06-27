4362b9ec0f67a2e51c841db67bbd350a86c88456
Ralf Stockmann <rstockm@gmail.com>
2020-06-27 02:41:28 +0200

    Export
    Drag&Drop und Preset-Slots umgesetzt, noch nicht performance-optimiert

--- Scripts/ultraschall_export_assistant.lua
+++ Scripts/ultraschall_export_assistant.lua
@@ -39,10 +39,18 @@ function runcommand(cmd)     -- run a command by its name
 end
 
 function GetPath(str,sep)
-
     return str:match("(.*"..sep..")")
-
 end
+
+function GetFileName(url)
+	return url:match("^.+/(.+)$")
+end
+
+function GetFileExtension(url)
+  return url:match("^.+(%..+)$")
+end
+
+
 
 local function splitWords(Lines, limit)
   while #Lines[#Lines] > limit do
@@ -170,9 +178,66 @@ function checkImage()
 
 end
 
-function GetFileExtension(url)
-  return url:match("^.+(%..+)$")
+
+function checkSlot(path)
+
+	-- lookup existing episode image
+
+	img_index = false
+	found = false
+	image_slot_path = false
+
+	endings = {".jpg", ".jpeg", ".png"} -- prefer .png
+	for key,value in pairs(endings) do
+		img_test = gfx.loadimg(0, path .. value)
+		if img_test ~= -1 then
+			img_index = img_test
+			image_slot_path = path .. value
+		end
+	end
+
+	if img_index then     -- there is an image
+		found = true
+		preview_size = 37      -- preview size in Pixel, always square
+		w, h = gfx.getimgdim(img_index)
+		if w > h then     -- adjust size to the longer border
+			img_ratio = preview_size / w
+		else
+			img_ratio = preview_size / h
+		end
+	end
+
+	return found, image_slot_path, img_ratio, img_name
+
 end
+
+
+
+function DeleteImages(url)
+	os.remove(url..".jpg")
+	os.remove(url..".jpeg")
+	os.remove(url..".png")
+end
+
+function GetDropzone(x,y)
+
+	for i = 1, #dropzone do
+		if x > dropzone[i].x and x < dropzone[i].x + 38 and y > dropzone[i].y and y < dropzone[i].y + 38 then
+			return i
+		end
+	end
+
+	return 0
+end
+
+function UsePresetCover(preset_path)
+
+	destination_path = dir .. "cover" .. GetFileExtension(preset_path)
+	DeleteImages(dir .. "cover")
+	retval = ultraschall.MakeCopyOfFile_Binary(preset_path, destination_path)
+
+end
+
 
 
 -- initiate values
@@ -182,7 +247,7 @@ script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
 GUI = dofile(script_path .. "ultraschall_gui_lib.lua")
 gfx_path = script_path.."/Ultraschall_Gfx/Soundcheck/"
 header_path = script_path.."/Ultraschall_Gfx/Headers/"
-
+cover_path = script_path.."/Ultraschall_Gfx/Covers/"
 
 
 GUI.name = "Ultraschall Export Assistant"
@@ -195,6 +260,22 @@ __, __, screen_w, screen_h = reaper.my_getViewport(l, t, r, b, l, t, r, b, 1)
 GUI.x, GUI.y = (screen_w - GUI.w) / 2, (screen_h - GUI.h) / 2
 
 y_offset = -30  -- move all content up/down
+
+-- dropzones
+
+dropzone = {}
+dropzone[1] = {}
+dropzone[1]["x"] = 870
+dropzone[1]["y"] = 391
+dropzone[2] = {}
+dropzone[2]["x"] = 870
+dropzone[2]["y"] = 432
+dropzone[3] = {}
+dropzone[3]["x"] = 911
+dropzone[3]["y"] = 391
+dropzone[4] = {}
+dropzone[4]["x"] = 911
+dropzone[4]["y"] = 432
 
 -- OS BASED SEPARATOR
 
@@ -306,13 +387,29 @@ function buildGUI()
 
 
 	changed, num_dropped_files, dropped_files, drop_mouseposition_x, drop_mouseposition_y = ultraschall.GFX_GetDropFile()
-	if num_dropped_files > 0 then
+	if num_dropped_files > 0 and changed then
 		-- print ("da: "..dropped_files[1])
+
+		dropzone_id = GetDropzone(drop_mouseposition_x/dpi_scale, (drop_mouseposition_y/dpi_scale)-y_offset)
 		source_file = dropped_files[1]
 		extension = (GetFileExtension(source_file))
-		destination_file = dir .. "cover" .. extension
+
+		if dropzone_id ~= 0 then
+
+			-- print(dropzone_id)
+			destination_file = cover_path .. tostring(dropzone_id) .. extension
+			DeleteImages(cover_path .. tostring(dropzone_id))
+
+		else
+
+			destination_file = dir .. "cover" .. extension
+			DeleteImages(dir .. "cover")
+			--print (drop_mouseposition_x/dpi_scale.."-".. drop_mouseposition_y/dpi_scale)
+
+		end
 
 		retval = ultraschall.MakeCopyOfFile_Binary(source_file, destination_file)
+
 	else
 		-- print ("nix"..tostring(changed))
 	end
@@ -350,11 +447,38 @@ function buildGUI()
 		img_ratio = 1
 	end
 
-	print(logo_path)
+	-- print(logo_path)
 
 	logo = GUI.Pic:new(784,390+y_offset, 80, 80, img_ratio, logo_path, runcommand, "_Ultraschall_Open_Project_Folder")
 	table.insert (GUI.elms, logo)
 
+	if found then
+		logo_hover_path = header_path.."dropzone_large_hover.png"
+		logo2 = GUI.Pic:new(784,390+y_offset, 80, 80, 1, logo_hover_path, runcommand, "_Ultraschall_Open_Project_Folder")
+		table.insert (GUI.elms, logo2)
+	end
+
+	-- Preset Slots
+
+	logo_path = header_path.."dropzone_small.png"
+
+	for i = 1, #dropzone do
+
+		found, image_slot_path, img_ratio = checkSlot(cover_path .. i)
+
+		if found then
+
+			logo_slot = GUI.Pic:new(dropzone[i].x, dropzone[i].y + y_offset, 37, 37, img_ratio, image_slot_path, UsePresetCover, image_slot_path)
+			table.insert (GUI.elms, logo_slot)
+
+		else
+
+			logo_slot = GUI.Pic:new(dropzone[i].x, dropzone[i].y + y_offset, 38, 38, 1, logo_path, runcommand, "_Ultraschall_Open_Project_Folder")
+			table.insert (GUI.elms, logo_slot)
+
+		end
+
+	end
 
 
 	-- 5. Finalize
