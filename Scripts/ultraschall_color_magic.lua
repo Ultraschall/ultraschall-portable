ca56fafe432658eaa85967b5759bab3ec1a95cbb
Ralf Stockmann <rstockm@gmail.com>
2020-12-02 01:55:04 +0100

    Update ultraschall_color_magic.lua
    Basisfunktionalität

--- Scripts/ultraschall_color_magic.lua
+++ Scripts/ultraschall_color_magic.lua
@@ -37,8 +37,10 @@ function getNextColor (LastColor)
 
   if ColorPosition == nil then
     ColorPosition = 0
+  elseif ColorPosition > 9 then
+    ColorPosition = ColorPosition - 9
   else
-    ColorPosition = ColorPosition + 3
+    ColorPosition = ColorPosition + 2
   end
 
   return t[ColorPosition]
@@ -57,7 +59,39 @@ function swapColors (color)
 
 end
 
+
+
+
+function countColoredStudioLinkTracks ()
+
+  local count = 0
+  for i=1, numberOfTracks do
+
+    local tracktype = ultraschall.GetTypeOfTrack(i)
+    -- print (tracktype)
+    if tracktype == "StudioLink" then
+
+      local MediaTrack = reaper.GetTrack(0, i-1)
+      local TrackColor = reaper.GetTrackColor(MediaTrack)
+      -- print (TrackColor)
+      if TrackColor ~= 0 then
+        count = count +1
+      end
+    end
+  end
+
+  return count
+end
+
+
+
+
 -- Init
+
+local _, left, top, right, bottom = reaper.JS_Window_GetClientRect( reaper.JS_Window_FindChildByID( reaper.GetMainHwnd(), 1000) )
+height = bottom - top
+
+print (height)
 
 -----------------------
 -- Step 1 : get started
@@ -94,15 +128,17 @@ end
 
 for key,value in pairs(t_invert) do
 
-  print (key.."-"..value)
+  -- print (key.."-"..value)
 
 end
 
 
 
 LastColor = 0
+StudioLinkNr = 0
+colored = 0
 
-print ("-")
+-- print ("-")
 
 
 
@@ -118,18 +154,32 @@ for i=0, numberOfTracks-1 do
 
   if TrackColor == 0 then -- frische Spur, noch keine Farbe gesetzt
 
-    -- LastColor = swapColors(LastColor)
+    tracktype = ultraschall.GetTypeOfTrack(i+1)
+    -- print (tracktype)
 
-    -- print ("last: "..LastColor)
+    if tracktype == "SoundBoard" then
 
-    NewTrackColor = getNextColor(LastColor)
+      soundboardColor = reaper.ColorToNative(100, 100, 100)
+      reaper.SetTrackColor(MediaTrack, soundboardColor)
 
-    r, g, b = reaper.ColorFromNative(NewTrackColor)
-    -- print ("NewTrack: "..NewTrackColor.."-"..r.."-"..g.."-"..b)
-    -- NewTrackColor = reaper.ColorToNative(b, g, r)
+    elseif tracktype == "StudioLink" then
 
-    reaper.SetTrackColor(MediaTrack, swapColors(NewTrackColor))
-    TrackColor = NewTrackColor
+      colored = countColoredStudioLinkTracks() or 0
+      print (colored)
+      StudioLinkColor = t[colored + 11]
+      reaper.SetTrackColor(MediaTrack, swapColors(StudioLinkColor))
+      StudioLinkNr = StudioLinkNr + 1
+
+    else -- normaler Track
+
+      NewTrackColor = getNextColor(LastColor)
+      r, g, b = reaper.ColorFromNative(NewTrackColor)
+      -- print ("NewTrack: "..NewTrackColor.."-"..r.."-"..g.."-"..b)
+      -- NewTrackColor = reaper.ColorToNative(b, g, r)
+
+      reaper.SetTrackColor(MediaTrack, swapColors(NewTrackColor))
+      TrackColor = NewTrackColor
+    end
   end
 
   LastColor = TrackColor
@@ -137,107 +187,3 @@ for i=0, numberOfTracks-1 do
 
 
 end
-
-
-
-
-
-
-
-
---[[
-
-
-
-
-function buildRoutingMatrix ()
-
-	local AllMainSends, number_of_tracks = ultraschall.GetAllMainSendStates()
-	local AllAUXSendReceives, number_of_tracks = ultraschall.GetAllAUXSendReceives()
-
-	soundbed_mix = tonumber(ultraschall.GetUSExternalState("ultraschall_settings_preshow_mix", "Value" ,"ultraschall-settings.ini")) -- wie hoch soll der Anteil des Soundbards während der Preshow im Monitormix sein
-
---	 print(serialize(AllMainSends))
-
-  for i=1, number_of_tracks do
-
-  	tracktype = ultraschall.GetTypeOfTrack(i)
-
-    if tracktype == "StudioLink" then	-- Behandlung der StudioLink Spuren
-
-    	retval = ultraschall.AddTrackHWOut(i, 0, 0, 1, 0, 0, 0, 0, -1, 0, false) -- StudioLink-Spuren gehen immer auf den MainHardwareOut Zurück
-
-    	for j=1, number_of_tracks do
-
-    		if ultraschall.GetTypeOfTrack(j) ~= "StudioLink" then
-
-					-- boolean retval = ultraschall.AddTrackAUXSendReceives(integer tracknumber, integer recv_tracknumber, integer post_pre_fader, number volume, number pan, integer mute, integer mono_stereo, integer phase, integer chan_src, integer snd_chan, number unknown, integer midichanflag, integer automation, boolean undo)
-					if ultraschall.GetTypeOfTrack(j) == "SoundBoard" then
-						send_volume = soundbed_mix
-					else
-						send_volume = 1
-					end
-
-    			setstate = ultraschall.AddTrackAUXSendReceives(i, j, 0, send_volume, 0, 0, 0, 0, 0, 0, -1, 0, 0, false)
-    			-- print(i.j)
-    		end
-			end
-
-    elseif tracktype == "SoundBoard" then	-- Behandlung der Soundboard Spuren
-
-			AllMainSends[i]["MainSendOn"] = 1 -- Bei der Preshow sendet nur das Soundboard auf den Main
-
-      retval = ultraschall.AddTrackHWOut(i, 0, 0, soundbed_mix, 0, 0, 0, 0, -1, 0, false) -- Soundboard-Spuren gehen immer auf den MainHardwareOut Zurück
-
-		else -- normaler, lokaler Track
-
-			retval, actual_device_name = reaper.GetAudioDeviceInfo("IDENT_IN", "")
-
-			if tonumber(ultraschall.GetUSExternalState("ultraschall_devices",actual_device_name,"ultraschall-settings.ini")) == 0 then  -- Audio-Device kann kein lokales Monitoring
-				retval = ultraschall.AddTrackHWOut(i, 0, 0, 1, 0, 0, 0, 0, -1, 0, false) -- sendet auch auf den HWOut in den Kopfhörer
-			end
-
-		end
-  end
-
-  retval = ultraschall.ApplyAllMainSendStates(AllMainSends)	-- setze alle Sends zum Master
-
-end
-
-
--- profile.start()
-
-retval = ultraschall.ClearRoutingMatrix(true, true, true, true, false)
-
-if reaper.CountTracks(0) > 0 then
-  buildRoutingMatrix ()
-end
-
-reaper.SetProjExtState(0, "ultraschall_magicrouting", "step", "preshow")
-
-is_new,name,sec,cmd,rel,res,val = reaper.get_action_context()
-state = reaper.GetToggleCommandStateEx(sec, cmd)
-
-ID_1 = reaper.NamedCommandLookup("_Ultraschall_set_Matrix_Preshow") -- Setup Button
-ID_2 = reaper.NamedCommandLookup("_Ultraschall_set_Matrix_Recording") -- Record Button
-ID_3 = reaper.NamedCommandLookup("_Ultraschall_set_Matrix_Editing") -- Edit Button
-
-if state <= 0 then
-  reaper.SetToggleCommandState(sec, cmd, 1)
-end
-
-reaper.SetToggleCommandState(sec, ID_2, 0)
-reaper.SetToggleCommandState(sec, ID_3, 0)
-reaper.SetProjExtState(0, "gui_statemanager", "_Ultraschall_set_Matrix_Editing", 0)
-reaper.SetProjExtState(0, "gui_statemanager", "_Ultraschall_set_Matrix_Recording", 0)
-reaper.SetProjExtState(0, "gui_statemanager", "_Ultraschall_set_Matrix_Preshow", 1)
-
-reaper.RefreshToolbar2(sec, cmd)
-
-
--- profile.stop()
--- report for the top 10 functions, sorted by execution time
--- local r = profile.report('time', 10)
--- print(r)
-
-]]
