a24b58fc958522f92afab61e6ad96b1472fe3219
Ralf Stockmann <rstockm@gmail.com>
2021-03-26 10:47:26 +0100

    Update ultraschall_align_items.lua
    Richtige Anzahl an Items wird auch dann ausgegeben, wenn noch weitere Tracks unter den beiden zu bearbeitenden sind.

--- Scripts/ultraschall_align_items.lua
+++ Scripts/ultraschall_align_items.lua
@@ -1,7 +1,7 @@
 --[[
 ################################################################################
 #
-# Copyright (c) 2014-2020 Ultraschall (http://ultraschall.fm)
+# Copyright (c) 2014-2021 Ultraschall (http://ultraschall.fm)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,6 @@ dofile(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")
 
 function abort(txt)
 
-
   result = reaper.ShowMessageBox( txt, "FAILURE", 0 )
 
 end
@@ -42,7 +41,6 @@ function check_items()
   local txt = ""
 
   for i = 1, number_of_tracks do
-
 
     count, MediaItemArray, MediaItemStateChunkArray = ultraschall.GetAllSelectedMediaItemsBetween(0, reaper.GetProjectLength(), tostring(i), false)
 
@@ -72,7 +70,6 @@ function check_items()
     success_state = false 
   end
 
-
   return success_state, txt
 end
 
@@ -80,9 +77,10 @@ function main()
 
   state, txt = check_items()
   if state == true then
+    countAligned = string.sub(tostring(reaper.CountSelectedMediaItems(0) / 2), 1, -3)
     commandid = reaper.NamedCommandLookup("_X-Raym_align")
     reaper.Main_OnCommand(commandid,0)         
-    result = reaper.ShowMessageBox( count.." items were aligned.", "SUCCESS", 0 )
+    result = reaper.ShowMessageBox( countAligned .." items were aligned.", "SUCCESS", 0 )
   else
     abort(txt)
   end
