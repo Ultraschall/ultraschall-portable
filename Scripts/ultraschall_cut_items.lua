6874e2396d6a150b35e8210b34e956ab49a3d025
Ralf Stockmann <rstockm@gmail.com>
2021-03-29 23:50:46 +0200

    Update ultraschall_cut_items.lua
    Belegung der Del Taste optimiert

--- Scripts/ultraschall_cut_items.lua
+++ Scripts/ultraschall_cut_items.lua
@@ -55,6 +55,35 @@ if playstate&4==4 then return end -- quit if recording
 reaper.Undo_BeginBlock() -- Beginning of the undo block. Leave it at the top of your main function.
 -------------------------------------
 
+
+init_start_timesel, init_end_timesel = reaper.GetSet_LoopTimeRange(0, 0, 0, 0, 0)  -- get information wether or not a 
+
+if (init_end_timesel ~= init_start_timesel) then    -- there is a time selection
+
+  
+  cut1 = reaper.NamedCommandLookup("_Ultraschall_Split_Items_Timeselection") -- Start preview 
+  reaper.Main_OnCommand(cut1, 0)
+  
+  cut1 = reaper.NamedCommandLookup("_Ultraschall_Remove_Items_Tracks_Envelopepoints_Depending_On_Focus") -- Start preview 
+  reaper.Main_OnCommand(cut1, 0)
+  
+
+else
+  Length=reaper.GetProjectLength()
+  reaper.Main_OnCommand(40697, 0) -- normal delete
+
+  Length2=reaper.GetProjectLength()
+
+  Diff=Length2-Length
+
+  if Diff~=0 and reaper.GetPlayState()~=0 and reaper.GetCursorContext()==1 and reaper.GetToggleCommandState(40311)==1 then
+    reaper.SetEditCurPos(reaper.GetPlayPosition()+Diff, false, true)
+  end
+end
+
+
+--[[
+
 selected_items_count = reaper.CountSelectedMediaItems(0)
 
 if selected_items_count > 0 then  -- at least one item selected
@@ -74,8 +103,10 @@ else                           -- no items selected
    result = reaper.ShowMessageBox( "You need to select at least one item to ripple-cut.", "Ultraschall Ripple Cut", 0 )  -- Info window
 end
 
+]]
+
 reaper.UpdateArrange()
 
 -------------------------------------
-reaper.Undo_EndBlock("Ultraschall Ripple Cut", -1) -- End of the undo block. Leave it at the bottom of your main function.
+reaper.Undo_EndBlock("Ultraschall Delete", -1) -- End of the undo block. Leave it at the bottom of your main function.
 -------------------------------------
