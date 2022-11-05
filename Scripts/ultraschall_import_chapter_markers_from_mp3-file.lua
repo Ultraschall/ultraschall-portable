e81cfddeddce3dc99340714c3ce2daf727be29e3
Meo-Ada Mespotine <lspmp3@yahoo.de>
2022-11-05 11:31:58 +0100

    Import von Kapitelmarken von MP3-Files

---
+++ Scripts/ultraschall_import_chapter_markers_from_mp3-file.lua
@@ -0,0 +1,41 @@
+dofile(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")
+
+-- get filename and source
+retval, filename = reaper.GetUserFileNameForRead(reaper.GetExtState("Ultraschall_Import_Chapter_Markers", "MP3_Path"), "Choose file MP3-file to import chapters from...", "*.mp3")
+if retval==false then return end
+filename=string.gsub(filename, "\\", "/")
+src=reaper.PCM_Source_CreateFromFile(filename)
+
+-- store path to be used the next time this action is run as default path
+path=filename:match("(.*/)")
+if path~=reaper.GetExtState("Ultraschall_Import_Chapter_Markers", "MP3_Path") then
+  reaper.SetExtState("Ultraschall_Import_Chapter_Markers", "MP3_Path", path, true)
+end
+
+
+-- read all chapters available from file 
+local i=0 -- count variable
+local retval
+local Chaps={} -- table to get all chapternames, and their positions
+while Chap~="" do
+  i=i+1
+  i2=tostring(i)
+  if i2:len()==2 then i2="0"..i2
+  elseif i2:len()==1 then i2="00"..i2
+  end
+  retval, Chap = reaper.GetMediaFileMetadata(src, "ID3:CHAP"..i2)
+  if Chap=="" then break end
+
+  Chaps[i]={tonumber(Chap:match("(.-):")), tonumber(Chap:match(".-:(.-):")), Chap:match(".-:.-:(.*)")}
+end
+
+-- when no chapters, show error-message
+if #Chaps==0 then reaper.MB("No Chapter markers available.", "No chapter markers", 0) return end
+
+-- insert chapters and renumerate the marker-numbers
+for i=1, #Chaps do
+  marker_number, guid, normal_marker_idx = ultraschall.AddNormalMarker(Chaps[i][1]/1000, 0, Chaps[i][3])
+end
+ultraschall.RenumerateNormalMarkers()
+
+reaper.UpdateArrange()
