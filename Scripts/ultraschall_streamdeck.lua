fc9f8488188b4b74beb4629fb9930c1f7cb29205
Meo-Ada Mespotine <lspmp3@yahoo.de>
2023-08-20 18:17:45 +0200

    Update ultraschall_streamdeck.lua

--- Scripts/ultraschall_streamdeck.lua
+++ Scripts/ultraschall_streamdeck.lua
@@ -26,9 +26,15 @@
 
 operationSystem = reaper.GetOS()
 if string.match(operationSystem, "OS") then
+  if reaper.file_exists(os.getenv("HOME").."/Library/Application Support/com.elgato.StreamDeck/Plugins/fm.ultraschall.ultradeck.sdPlugin/LUA/Ultraschall_StreamDeck_2.lua")==false then
+    reaper.MB("Stream Deck-software not installed.", "Not installed", 0)
+  end
   dofile(os.getenv("HOME").."/Library/Application Support/com.elgato.StreamDeck/Plugins/fm.ultraschall.ultradeck.sdPlugin/LUA/Ultraschall_StreamDeck_2.lua")
 elseif string.sub(reaper.GetOS(),1,3)=="Win" then
   dofile(os.getenv("APPDATA")..[[\Elgato\StreamDeck\Plugins\com.elgato.controlcenter.sdPlugin\Ultraschall_StreamDeck_2.lua]])
+  if reaper.file_exists(os.getenv("APPDATA")..[[\Elgato\StreamDeck\Plugins\com.elgato.controlcenter.sdPlugin\Ultraschall_StreamDeck_2.lua]])==false then
+    reaper.MB("Stream Deck-software not installed.", "Not installed", 0)
+  end
 else
   reaper.ShowMessageBox("Stream Deck Plugin is only available on MacOS and Windows", "Wrong OS", 0)
 end
